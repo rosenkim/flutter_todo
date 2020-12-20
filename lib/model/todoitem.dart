f5888474e5a77f0f34b92ba49b35371c65e15922
rosenkim <rosenkimkr@gmail.com>
2020-12-20 20:49:52 +0900

    swipe 동작으로 리스트 삭제 기능 추가 (Add a list deletion with the swipe action)

--- lib/model/todoitem.dart
+++ lib/model/todoitem.dart
@@ -1,4 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
 class TodoItem {
+  TodoItem() {
+    _key = UniqueKey().toString();
+  }
+
+  String _key; // 키
+  String get key {
+    return _key;
+  }
+
   String title; // 제목
   String memo; // 메모
   DateTime datetime; // 날짜
--- lib/todolist.dart
+++ lib/todolist.dart
@@ -30,26 +30,88 @@ class _TodoListState extends State<TodoList> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        var item = items[index];
-        var textStyle = item.isDone
-            ? TextStyle(decoration: TextDecoration.lineThrough)
-            : null;
-
-        return ListTile(
-          title: Text('$index : ${item.title}', style: textStyle),
-          subtitle: Text('${item.memo}', style: textStyle),
-          onTap: () {
-            setState(() {
-              item.isDone = !item.isDone;
-            });
-          },
+  // https://stackoverflow.com/questions/55777213/flutter-how-to-use-confirmdismiss-in-dismissible
+  // 를 참고하여 아래 함수 및 Dismissible의 confirmDismiss 이벤트를 구현
+  Future<bool> _showConfirmationDialog(BuildContext context, String action) {
+    return showDialog<bool>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Do you want to $action this?'),
+          actions: <Widget>[
+            FlatButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                Navigator.pop(context, true); // showDialog() returns true
+              },
+            ),
+            FlatButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.pop(context, false); // showDialog() returns false
+              },
+            ),
+          ],
         );
       },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          var item = items[index];
+          var textStyle = item.isDone
+              ? TextStyle(decoration: TextDecoration.lineThrough)
+              : null;
+
+          return Dismissible(
+              key: Key(item.key),
+              confirmDismiss: (DismissDirection direction) async {
+                switch (direction) {
+                  case DismissDirection.endToStart:
+                    break;
+                  case DismissDirection.startToEnd:
+                    return await _showConfirmationDialog(context, 'Delete') ==
+                        true;
+                  case DismissDirection.horizontal:
+                  case DismissDirection.vertical:
+                  case DismissDirection.up:
+                  case DismissDirection.down:
+                    assert(false);
+                    break;
+
+                  case DismissDirection.none:
+                    // 아무것도 하지 않음.
+                    break;
+                }
+                return false;
+              },
+              background: Container(
+                padding: EdgeInsets.symmetric(horizontal: 12.0),
+                color: Colors.redAccent,
+                alignment: Alignment.centerLeft,
+              ),
+              onDismissed: (direction) {
+                if (direction == DismissDirection.startToEnd) {
+                  setState(() {
+                    if (item != null) {
+                      items.remove(item);
+                    }
+                  });
+                }
+              },
+              child: ListTile(
+                  title: Text('$index : ${item.title}', style: textStyle),
+                  subtitle: Text('${item.memo}', style: textStyle),
+                  onTap: () {
+                    setState(() {
+                      item.isDone = !item.isDone;
+                    });
+                  }));
+        });
   }
 }
